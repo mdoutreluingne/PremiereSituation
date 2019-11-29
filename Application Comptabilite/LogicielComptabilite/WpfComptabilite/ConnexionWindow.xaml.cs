a883e83a7e165c1eb31d21d5729f1e41900389ee
Maxime <maxime.doutr@sfr.fr>
2019-11-29 18:30:15 +0100

    Fenêtre de connexion finit

--- Application Comptabilite/LogicielComptabilite/WpfComptabilite/ConnexionWindow.xaml.cs
+++ Application Comptabilite/LogicielComptabilite/WpfComptabilite/ConnexionWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace WpfComptabilite
         //static private dbal bdd = new dbal("admin", "admin", 3306, "172.31.135.2", "bdd_escape_game");
         static private dbal bdd = new dbal("root", "", 3306, "127.0.0.1", "bdd_escape_game");
         static private daoUtilisateur unDaoUtilisateur = new daoUtilisateur(bdd);
+        static private List<Utilisateur> lesUsers = new List<Utilisateur>(unDaoUtilisateur.selectAllClient());
 
         public ConnexionWindow()
         {
@@ -34,9 +35,10 @@ namespace WpfComptabilite
         }
         //PERSONNE NE TOUCHE AU CODE
         private void connecter_Click(object sender, RoutedEventArgs e)
-        {
-            List<Utilisateur> lesUsers = new List<Utilisateur>(unDaoUtilisateur.selectAllClient());
+        {  
             string login = cmb_ville.Text + txt_login.Text;
+            bool erreur_login = false;
+            bool erreur_pass = false;
 
             foreach (Utilisateur user in lesUsers)
             {
@@ -47,20 +49,97 @@ namespace WpfComptabilite
                         MainWindow logicielCompta = new MainWindow();
                         logicielCompta.Show(); //Ouvre la fenetre
                         this.Close(); //Fermeture de la fenêtre actuel
+                        erreur_login = false;
+                        erreur_pass = false;
+                        break;
                     }
                     else
                     {
-                        MessageBox.Show("Mot de passe incorrect", "Erreur de connexion", MessageBoxButton.OK, MessageBoxImage.Error);
+                        erreur_login = false;
+                        erreur_pass = true;
+                        break;
                     }
                 }
                 else
                 {
-                    MessageBox.Show("Login incorrect", "Erreur de connexion", MessageBoxButton.OK, MessageBoxImage.Error);
+                    erreur_login = true;
                 }
             }
 
+            //Gestion cas d'erreur
+            if (erreur_login == true)
+            {
+                MessageBox.Show("Login incorrect", "Erreur de connexion", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (erreur_pass == true)
+            {
+                MessageBox.Show("Mot de passe incorrect", "Erreur de connexion", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
         }
-        
+
+        private void btn_modif_pass_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_comfirm_pass.Visibility = Visibility.Visible;
+            pass_confirm.Visibility = Visibility.Visible;
+            btn_connecter.Visibility = Visibility.Hidden;
+            btn_update.Visibility = Visibility.Visible;
+
+        }
+
+        private void btn_update_Click(object sender, RoutedEventArgs e)
+        {
+            string login = cmb_ville.Text + txt_login.Text;
+            bool erreur_login = false;
+            bool erreur_pass = false;
+
+            if (txt_login.Text != "" || pass.Password != "")
+            {
+                foreach (Utilisateur user in lesUsers)
+                {
+                    if (login.Contains(user.Login) == true)
+                    {
+                        if (pass.Password == pass_confirm.Password)
+                        {
+                            unDaoUtilisateur.update("UPDATE utilisateur SET mdp = '" + pass.Password + "' WHERE login = '" + login + "'");
+                            lbl_comfirm_pass.Visibility = Visibility.Hidden;
+                            pass_confirm.Visibility = Visibility.Hidden;
+                            btn_connecter.Visibility = Visibility.Visible;
+                            btn_update.Visibility = Visibility.Hidden;
+                            erreur_login = false;
+                            lesUsers = new List<Utilisateur>(unDaoUtilisateur.selectAllClient()); //Rechargement des utilisateurs après modification mdp
+                            break;
+
+                        }
+                        else
+                        {
+                            erreur_login = false;
+                            erreur_pass = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        erreur_login = true;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez renseigner les champs", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            //Gestion cas d'erreur
+            if (erreur_login == true)
+            {
+                MessageBox.Show("Login incorrect", "Erreur de connexion", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (erreur_pass == true)
+            {
+                MessageBox.Show("Les mots de passe saisie ne sont pas identiques", "Erreur de connexion", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
 
     }
 }
